3674ba669a36096773213ff03be020c207b87aad
vromanie <vromanie@amazon.lu>
2021-09-08 11:18:46 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import ReactDOM from "react-dom";
-import "./index.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Header from "./header";
-import Footer from "./footer";
-
 Amplify.configure(config);
 
 ReactDOM.render(
@@ -17,22 +13,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const routing = (
-  <Router>
-    <div>
-      <Header />
-      <hr />
-      <Switch>
-        <Route exact path="/" component={App} />
-      </Switch>
-      <Footer />
-    </div>
-  </Router>
-);
-
-ReactDOM.render(routing, document.getElementById("root"));
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
